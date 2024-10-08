bb23a48aeb18413a4e2a65066a9543a303742cf5
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-10-08 16:17:29 +0700

    build(pages): add github actions deploy build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tzeroocne.github.io',
+  base: '/nnry-astro',
   server: {
     port: 7673,
   },
